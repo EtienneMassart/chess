9aab3e58eeef66a1d606d28c29c54f8b233ffa00
massart.etienne@gmail.com <massart.etienne@gmail.com>
2025-06-10 12:01:58 +0200

    Add receive loop on server

--- chess_server/src/main.rs
+++ chess_server/src/main.rs
@@ -13,8 +13,21 @@ async fn main() -> tokio::io::Result<()> {
     let listener = TcpListener::bind(SERVER_ADDRESS).await?;
     println!("Server listening on {SERVER_ADDRESS}...");
     
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        println!("New connection from {}", addr);
 
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
 
-
-    Ok(())
+            match socket.read(&mut buffer).await {
+                Ok(n) if n > 0 => {
+                    println!("Received: {:?}", &buffer[..n]);
+                    socket.write_all(b"Message received").await.unwrap();
+                }
+                Ok(_) => println!("Connection closed by client"),
+                Err(e) => eprintln!("Error reading from socket: {}", e),
+            }
+        });
+    }
 }
